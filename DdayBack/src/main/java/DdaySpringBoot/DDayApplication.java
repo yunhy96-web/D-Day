2a9e1c5259539167a59e33c0bf3198beeaa6e674
yunhy96 <yhy0818@gmail.com>
2023-11-25 00:20:02 +0900

    2023 11 25 초기환경세팅

---
+++ DdayBack/src/main/java/DdaySpringBoot/DDayApplication.java
@@ -0,0 +1,11 @@
+package DdaySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DDayApplication.class, args);
+    }
+}
