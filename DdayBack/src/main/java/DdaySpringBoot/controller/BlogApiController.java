708897590840e9489c4464b45eda0ff87184b37b
yunhy96 <yhy0818@gmail.com>
2023-12-04 23:59:34 +0900

    dd

--- DdayBack/src/main/java/DdaySpringBoot/controller/BlogApiController.java
+++ DdayBack/src/main/java/DdaySpringBoot/controller/BlogApiController.java
@@ -2,13 +2,15 @@ package DdaySpringBoot.controller;
 
 import DdaySpringBoot.domain.Article;
 import DdaySpringBoot.dto.AddArticleRequest;
+import DdaySpringBoot.dto.ArticleResponse;
+import DdaySpringBoot.dto.UpdateArticleRequest;
 import DdaySpringBoot.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,10 +18,51 @@ public class BlogApiController {
 
     private final BlogService blogService;
 
+    //글 등록
     @PostMapping("/api/articles")
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
     }
+    //전체 조회 여기에 id도 출력해줘야 나중에 클릭시 id 조회 가능.
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+    //id 기반으로 하나 조회
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<Article> findArticle(@PathVariable long id){
+        try {
+            Article article = blogService.findById(id);
+            return ResponseEntity.ok().body(article);
+        } catch (IllegalArgumentException e) {
+            // 클라이언트가 존재하지 않는 ID를 전송한 경우
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    //글삭제
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id){
+        blogService.delete(id);
+        return ResponseEntity.ok()
+                .build();
+    }
+    //굴 수정
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+        @RequestBody UpdateArticleRequest request){
+        Article updatedArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedArticle);
+    }
+
+
+
 
 }
---
+++ DdayBack/src/main/java/DdaySpringBoot/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package DdaySpringBoot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
