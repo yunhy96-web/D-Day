8f9f5d11fa582bd4d724fc35f1ae41bb955851ba
yunhy96 <yhy0818@gmail.com>
2023-12-07 23:14:48 +0900

    정상 코드

--- DdayBack/src/main/java/DdaySpringBoot/domain/User.java
+++ DdayBack/src/main/java/DdaySpringBoot/domain/User.java
@@ -4,9 +4,6 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
@@ -15,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Entity
-public class User implements UserDetails {
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name ="id", updatable = false)
@@ -33,41 +30,5 @@ public class User implements UserDetails {
         this.password = password;
     }
     //여기 아래 부분들이 다 유저디테일들
-    @Override //권한 반환
-    public Collection<? extends GrantedAuthority> getAuthorities(){
-        return List.of(new SimpleGrantedAuthority("user"));
-    }
 
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    //계정 만료 여부
-    @Override
-    public boolean isAccountNonExpired() {
-        return true; //만료되지 않음
-    }
-
-    //계정 잠금 여부
-    @Override
-    public boolean isAccountNonLocked() {
-        return true; // 잠금되지 않음
-    }
-
-    //패스워드의 만료 여부 반환
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;// 만료되지 않음
-    }
-    //계정 사용 가능 여부 반환
-    @Override
-    public boolean isEnabled() {
-        return true; // 계정 사용 가능
-    }
 }
